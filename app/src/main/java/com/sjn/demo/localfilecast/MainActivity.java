da5b496edab82ace7df46f6752f194e9ae44307f
Shinya KAWAKAMI <Shinya.Kawakami@so-net.co.jp>
2017-02-14 16:13:49 +0900

    serve local media

--- app/src/main/java/com/sjn/demo/localfilecast/MainActivity.java
+++ app/src/main/java/com/sjn/demo/localfilecast/MainActivity.java
@@ -1,17 +1,25 @@
 package com.sjn.demo.localfilecast;
 
+import android.Manifest;
+import android.database.Cursor;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.Map;
 
 import fi.iki.elonen.NanoHTTPD;
+
+import static fi.iki.elonen.NanoHTTPD.Response.Status.OK;
 
 public class MainActivity extends AppCompatActivity {
     private HttpServer mHttpServer;
@@ -20,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
         String ip = getWifiAddress();
         int port = findOpenPort(ip, 8080);
         ((TextView) findViewById(R.id.ip_address)).setText("http://" + ip + ":" + port);
@@ -46,14 +55,25 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Response serve(IHTTPSession session) {
-            String msg = "<html><body><h1>Hello server</h1>\n";
-            Map<String, String> parms = session.getParms();
-            if (parms.get("username") == null) {
-                msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-            } else {
-                msg += "<p>Hello, " + parms.get("username") + "!</p>";
+            FileInputStream stream = null;
+            String mediaPath = fetchMedia();
+            if (mediaPath == null) {
+                return new Response("No music");
             }
-            return new Response(msg + "</body></html>\n");
+            try {
+                stream = new FileInputStream(mediaPath);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            return new Response(OK, "audio/mp3", stream);
+        }
+    }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
     }
 
@@ -86,5 +106,18 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             return true;
         }
+    }
+
+    private String fetchMedia() {
+        String mediaPath = null;
+        Cursor mediaCursor = getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                new String[]{MediaStore.Audio.Media.DATA},
+                MediaStore.Audio.Media.IS_MUSIC + " != 0", null, null);
+        if (mediaCursor != null && mediaCursor.moveToFirst()) {
+            mediaPath = mediaCursor.getString(0);
+            mediaCursor.close();
+        }
+        return mediaPath;
     }
 }
